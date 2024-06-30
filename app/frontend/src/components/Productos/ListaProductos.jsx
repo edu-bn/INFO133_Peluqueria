7713e50be25fd73f6948a3a12aa758113daea206
edu-bn <eduardo.barreran@gmail.com>
2024-06-30 12:25:18 -0400

    Productos en tabla

--- app/frontend/src/components/Productos/ListaProductos.jsx
+++ app/frontend/src/components/Productos/ListaProductos.jsx
@@ -1,8 +1,7 @@
-// src/components/ProductList.js
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 
-const ListaProductos = () => {
+const ListaProductos = ({ onProductosLoaded }) => {
   const [productos, setProductos] = useState([]);
 
   useEffect(() => {
@@ -11,18 +10,20 @@ const ListaProductos = () => {
       try {
         const response = await axios.get('http://localhost:3000/api/productos');
         setProductos(response.data);
+        // Llama a la función de callback para pasar los productos al componente padre
+        if (onProductosLoaded) {
+          onProductosLoaded(response.data);
+        }
       } catch (error) {
         console.error('Error al obtener productos:', error);
       }
     };
 
     getProductos();
-  }, []);
+  }, [onProductosLoaded]);
 
   return (
     <div>
-      <div>
-      
       <ul className='lista_productos'>
         {productos.map((producto) => (
           <li key={producto.id_producto}>
@@ -30,7 +31,6 @@ const ListaProductos = () => {
           </li>
         ))}
       </ul>
-      </div>
     </div>
   );
 };
--- app/frontend/src/components/Productos/Productos.jsx
+++ app/frontend/src/components/Productos/Productos.jsx
@@ -1,15 +1,53 @@
+import { useState } from 'react';
 import ListaProductos from "./ListaProductos.jsx";
 import Top from "../Top.jsx";
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+} from '@chakra-ui/react'
 
 const Productos = () => {
-    return (
-      <div>
-        <Top text={'Productos'}></Top>
-        
-        <ListaProductos />
-      </div>
-    );
+  const [productos, setProductos] = useState([]);
+
+  // Función para recibir los productos desde ListaProductos
+  const handleProductosLoaded = (productos) => {
+    setProductos(productos);
   };
-  
-  export default Productos;
-  
+
+  return (
+    <div>
+      <Top text={'Productos'}></Top>
+
+      <TableContainer>
+        <Table variant='simple'>
+          <Thead>
+            <Tr>
+              <Th>Id producto</Th>
+              <Th>Nombre del producto</Th>
+              <Th isNumeric>Precio</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {productos.map((producto) => (
+              <Tr key={producto.id_producto}>
+                <Td>{producto.id_producto}</Td>
+                <Td>{producto.nombre}</Td>
+                <Td isNumeric>${producto.valor}</Td>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </TableContainer>
+
+      <ListaProductos onProductosLoaded={handleProductosLoaded} />
+    </div>
+  );
+};
+
+export default Productos;
